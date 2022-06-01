a80ed27ede5f8bcd75b38c32ec20cb599cd18b3a
Bob Funk <bobfunk11@gmail.com>
2022-05-31 20:21:11 -0500

    Add a scripted configuration to blocaled.
    Automate pulling info from slackware's scripts into a single
    /etc/locale.conf file.

--- slackbuilds/blocaled/doinst.sh
+++ slackbuilds/blocaled/doinst.sh
@@ -1,4 +1,2 @@
-# Generate an /etc/locale.conf file using the system's 
-# /etc/profile.d/lang.sh script:
-grep -v -E '^(#|$)' /etc/profile.d/lang.sh > /etc/locale.conf
-
+# Generate an /etc/locale.conf file using the supplied script:
+/usr/bin/configure-blocaled
